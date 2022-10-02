54797e24f92796f719cdda9043dea61e54c467a4
karthik <krk0241@gmail.com>
2022-10-02 14:00:31 -0400

    initial commit

---
+++ StudentData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+public class StudentData
+{
+	public string student_id = "";
+	public double student_score;
+
+	public StudentData(string str)
+	{
+		string[] strArr = str.Split(" ");
+        student_id = strArr[0];
+        student_score = Convert.ToDouble(strArr[1]);
+
+    }
+
+	/*private Queue<string> q = new Queue<string>();
+    public StudentData(string str)
+	{
+		str = str.Trim();
+		string[] strArr = str.Split(" ");
+		for(int i = 0; i < strArr.Length; i++)
+		{
+			q.Enqueue(strArr[i]);
+		}
+
+	}*/
+
+/*
+    public void getLine()
+	{
+        student_id = q.Peek();
+		q.Dequeue();	
+		student_score = Convert.ToDouble(q.Peek());
+		q.Dequeue();
+    }
+*/
+
+
+}
---
+++ TextDataHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+public class TextDataHandler
+{
+	
+	
+	private static StreamReader reader;
+
+	public TextDataHandler(String filename)
+	{
+
+		FileInfo fi = new FileInfo(filename);
+		reader = fi.OpenText();
+    }
+
+	public string read()
+	{
+		string data = "";
+		string str = "";
+
+		while( (data = reader.ReadLine()) != null)
+		{
+			//Console.WriteLine(data);
+			str = str + data + " ";
+		}
+		return str;
+	}
+    public void close()
+	{
+		reader.Close();
+	}
+
+
+}
